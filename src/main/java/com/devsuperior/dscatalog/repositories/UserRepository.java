b7f3a995039359749671997af216adc4522a7d8f
unknown <matheusquevedobio@gmail.com>
2024-02-19 21:15:18 -0300

    correção: postgres compatibilidade

--- src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.devsuperior.dscatalog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 import com.devsuperior.dscatalog.entities.User;
 
@@ -10,4 +11,5 @@ import com.devsuperior.dscatalog.entities.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByEmail(String email);
+	List<User> findByFirstName(String firstName);
 }
